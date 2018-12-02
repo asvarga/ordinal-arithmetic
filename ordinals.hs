769bd9cb310f84801ebc57fdac2a6e1e18105ee9
thealexvarga <thealexvarga@gmail.com>
2018-12-02 02:45:48 +0100

    proofs about subtraction and multiplication

--- ordinals.hs
+++ ordinals.hs
@@ -1,85 +1,34 @@
 
 -- An implementation of ordinal arithmetic
--- Liquid Haskell is used to require all Ordinals to be in Cantor Normal Form
-    -- Comments like {-@ ... @-} are for LH
-    -- To type-check with LH, run: liquid ordinals.hs
 
-----------------------------------------------------------------
-
-{-@ LIQUID "--reflection" @-}
-
---import Prelude hiding ((^))
---import qualified Prelude as P ((^))
-
-
-
-
-
-
-
-
-
---  TODO:
---    LH isn't using < on Ordinals at all
---    fix and use fromInteger
-
-
-
-
-
-
-
-
+import Prelude hiding ((^))
+import qualified Prelude as P ((^))
 
 ----------------------------------------------------------------
 
 -- (Ord a n b) = a^n + b
--- require Cantor Normal Form and use the measure "size" to check termination
--- the "v == Zero" is because degree is truncated to be >= Zero
-{-@ data Ordinal [size] 
-    = Ord { a :: Ordinal, 
-            n :: Nat,  
-            b :: {v:Ordinal | (compOrd (degree v) a == LT) || v == Zero } }
-    | Zero {} @-}
-data Ordinal = Ord Ordinal Integer Ordinal
+data Ordinal = Ord Ordinal Int Ordinal
              | Zero
              deriving Eq
 
-{-@ measure size @-}
-{-@ size :: Ordinal -> Nat @-}
-size :: Ordinal -> Integer
+size :: Ordinal -> Int
 size Zero = 1
 size (Ord a n b) = (size a) + 1 + (size b)
 
-{-@ measure degree @-}
-degree :: Ordinal -> Ordinal
-degree Zero = Zero              -- truncated
-degree (Ord a n b) = a
+normal :: Ordinal -> Bool
+normal Zero = True
+normal (Ord a n b) = (normal a) && (n > 0) && (normal b) && (case b of
+    Zero -> True
+    (Ord c _ _) -> (comp c a == LT))
 
-{-@ reflect compInt @-}
-{-@ compInt :: Integer -> Integer -> Ordering @-}
-compInt :: Integer -> Integer -> Ordering
-compInt x y = if x < y  then LT else if x == y then EQ else GT
+----------------------------------------------------------------
 
-{-@ reflect compOrd @-}
-compOrd :: Ordinal -> Ordinal -> Ordering
-compOrd Zero Zero = EQ
-compOrd Zero (Ord a n b) = LT
-compOrd (Ord a n b) Zero = GT
-compOrd (Ord a0 n0 b0) (Ord a1 n1 b1) =
-    case (compOrd a0 a1) of
-        LT -> LT
-        GT -> GT
-        EQ -> case (compInt n0 n1) of
-            LT -> LT
-            GT -> GT
-            EQ -> (compOrd b0 b1)
-
-instance Ord Ordinal where compare = compOrd
+-- to distinguish from Ints:
+natStyle x = x -- "[" ++ x ++ "]"
 
 instance Show Ordinal where
-    show Zero = "0"
-    show (Ord Zero n Zero) = (show n)
+    show Zero = natStyle "0"
+    show (Ord Zero n Zero) = natStyle (show n)
     show (Ord a n b) = (f a) ++ (g n) ++ (h b)
         where
             f (Ord Zero 1 Zero) = "ω"
@@ -92,109 +41,124 @@ instance Show Ordinal where
 
 ----------------------------------------------------------------
 
+instance Ord Ordinal where compare = comp
+
+compInt :: Int -> Int -> Ordering
+compInt x y = if x < y then LT else if x == y then EQ else GT
+
+comp :: Ordinal -> Ordinal -> Ordering
+comp Zero Zero = EQ
+comp Zero (Ord a n b) = LT
+comp (Ord a n b) Zero = GT
+comp (Ord a0 n0 b0) (Ord a1 n1 b1) =
+    case (a0 `comp` a1) of
+        LT -> LT
+        GT -> GT
+        EQ -> case (n0 `compInt` n1) of
+            LT -> LT
+            GT -> GT
+            EQ -> (b0 `comp` b1)
+
+maxOrd :: Ordinal -> Ordinal -> Ordinal
+maxOrd a b = case (a `comp` b) of
+    LT -> b
+    GT -> a
+    EQ -> a
+
+op :: Ordering -> Ordering
+op LT = GT
+op GT = LT
+op EQ = EQ
+
+----------------------------------------------------------------
 
 one = (Ord Zero 1 Zero) -- 1
 w   = (Ord one 1 Zero)  -- omega
 ω   = w
 
-
-
-
-
-
-
-
 -- Based On: https://en.wikipedia.org/wiki/Ordinal_arithmetic#Cantor_normal_form
 instance Num Ordinal where
-    (+) _ _ = 0
-    (-) _ _ = 0
-    (*) _ _ = 0
-    -- (+) x Zero = x
-    -- (+) Zero y = y
-    -- (+) (Ord a0 n0 b0) (Ord a1 n1 b1) = case (compare a0 a1) of
-    --     LT -> (Ord a1 n1 b1)
-    --     GT -> (Ord a0 n0 (b0+(Ord a1 n1 b1)))
-    --     EQ -> (Ord a0 (n0+n1) (b0+b1))
-    -- (-) x Zero = x
-    -- (-) Zero y = Zero
-    -- (-) (Ord a0 n0 b0) (Ord a1 n1 b1) = case (compare a0 a1) of
-    --     LT -> Zero
-    --     GT -> (Ord a0 n0 b0)
-    --     EQ -> case (compare n0 n1) of
-    --         LT -> Zero
-    --         GT -> (Ord a0 (n0-n1) b0)
-    --         EQ -> b0 - b1
-    -- (*) x Zero = Zero
-    -- (*) Zero x = Zero
-    -- (*) (Ord a0 n0 b0) (Ord Zero n1 Zero) = (Ord a0 (n0*n1) b0)
-    -- (*) (Ord a0 n0 b0) (Ord a1 n1 Zero) = (Ord (a0+a1) n1 Zero)
-    -- (*) x (Ord a1 n1 b1) = x*(Ord a1 n1 Zero) + x*b1
+    (+) x Zero = x
+    (+) Zero y = y
+    (+) (Ord a0 n0 b0) (Ord a1 n1 b1) = case (compare a0 a1) of
+        LT -> (Ord a1 n1 b1)
+        GT -> (Ord a0 n0 (b0+(Ord a1 n1 b1)))
+        EQ -> (Ord a0 (n0+n1) (b0+b1))
+    (-) x Zero = x
+    (-) Zero y = Zero
+    (-) (Ord a0 n0 b0) (Ord a1 n1 b1) = case (compare a0 a1) of
+        LT -> Zero
+        GT -> (Ord a0 n0 b0)
+        EQ -> case (compare n0 n1) of
+            LT -> Zero
+            GT -> (Ord a0 (n0-n1) b0)
+            EQ -> b0 - b1
+    (*) x Zero = Zero
+    (*) Zero x = Zero
+    (*) (Ord a0 n0 b0) (Ord Zero n1 Zero) = (Ord a0 (n0*n1) b0)
+    (*) (Ord a0 n0 b0) (Ord a1 n1 Zero) = (Ord (a0+a1) n1 Zero)
+    (*) x (Ord a1 n1 b1) = x*(Ord a1 n1 Zero) + x*b1
     abs x = x
     signum x = 1
     fromInteger 0 = Zero
-    fromInteger n = Ord Zero n Zero
+    fromInteger n = Ord Zero (fromIntegral n) Zero
 
--- (^) :: Ordinal -> Ordinal -> Ordinal                -- let 1 < n < ω:
--- (^) x Zero = one                                    -- x^0 = 1
--- (^) x (Ord Zero n Zero) = x P.^ n                   -- x^n uses Prelude's ^
--- (^) (Ord Zero 1 Zero) y = one                       -- 1^y = 1
--- (^) (Ord Zero n Zero) (Ord y 1 Zero) =              -- n^(ω^y) = ω^(ω^(y-1)) 
---     (Ord (Ord (y-one) 1 Zero) 1 Zero)               --   ex: 2^(ω^4) = ω^(ω^3)
--- (^) (Ord (Ord a n c) _ b) y =                       -- (ω^x)^y = ω^(x*y)
---     (Ord ((Ord a n c)*y) 1 Zero)                    --   ex: (ω^ω)^3 = ω^(ω*3)
--- (^) x (Ord y n Zero) = (x^(Ord y 1 Zero)) P.^ n     -- x^(y*n) = (x^y)^n
--- (^) x (Ord y n z) = x^(Ord y n Zero) * x^z          -- x^(y+z) = x^y * x^z
+(^) :: Ordinal -> Ordinal -> Ordinal                -- let 1 < n < ω:
+(^) x Zero = one                                    -- x^0 = 1
+(^) x (Ord Zero n Zero) = x P.^ n                   -- x^n uses Prelude's ^
+(^) (Ord Zero 1 Zero) y = one                       -- 1^y = 1
+(^) (Ord Zero n Zero) (Ord y 1 Zero) =              -- n^(ω^y) = ω^(ω^(y-1)) 
+    (Ord (Ord (y-one) 1 Zero) 1 Zero)               --   ex: 2^(ω^4) = ω^(ω^3)
+(^) (Ord (Ord a n c) _ b) y =                       -- (ω^x)^y = ω^(x*y)
+    (Ord ((Ord a n c)*y) 1 Zero)                    --   ex: (ω^ω)^3 = ω^(ω*3)
+(^) x (Ord y n Zero) = (x^(Ord y 1 Zero)) P.^ n     -- x^(y*n) = (x^y)^n
+(^) x (Ord y n z) = x^(Ord y n Zero) * x^z          -- x^(y+z) = x^y * x^z
 
--- --------
+--------
 
--- a = w^2*12 + w*3 + 5
--- b = w^2*12 + w*4 + 5
--- c = w^a*12 + w^2 + w*7 + 3
--- d = w^b*12 + w^2 + w*7 + 3
--- e = w^3 + w
--- f = w^5 + w^3
+a = w^2*12 + w*3 + 5
+b = w^2*12 + w*4 + 5
+c = w^a*12 + w^2 + w*7 + 3
+d = w^b*12 + w^2 + w*7 + 3
+e = w^3 + w
+f = w^5 + w^3
 
 main = do
-    print $ w
-    print $ degree one
-    -- print $ (degree one) < w
-    print $ Ord w 2 one
-
---     print $ (ω^3 + ω)^5
---     print $ (ω^5 + ω^3)^3
---     print $ "----"
---     print $ 1 + w
---     print $ c
---     print $ d
---     print $ c + d
---     print $ "----"
---     print $ a
---     print $ b
---     print $ b - a
---     print $ "----"
---     print $ (w^4*3 + w^3*2 + w^2 + w^1*10 + 100)^5
---     print $ (w^w + w^3*2 + w^2)^2
---     print $ (w+2)^0
---     print $ (w+2)^1
---     print $ (w+2)^2
---     print $ (w+2)^3
---     print $ (w+2)^4
---     print $ (w+2)^5
---     print $ "----"
---     print $ 2^0
---     print $ 2^w
---     print $ 2^(w*3 + 3)
---     print $ 2^(w^4 + w^3 + 5
---     print $ 2^(w^w)
---     print $ "----"
---     print $ w^(w^w)
---     print $ (w^w)^2
---     print $ (w^w)^w
---     print $ ((w^w)*3)^2
---     print $ (w^w)^(w^w)
---     print $ "----"
---     print $ (w*w+2)^w
---     print $ w*(w^2)
+    print $ (ω^3 + ω)^5
+    print $ (ω^5 + ω^3)^3
+    print $ "----"
+    print $ 1 + w
+    print $ c
+    print $ d
+    print $ c + d
+    print $ "----"
+    print $ a
+    print $ b
+    print $ b - a
+    print $ "----"
+    print $ (w^4*3 + w^3*2 + w^2 + w^1*10 + 100)^5
+    print $ (w^w + w^3*2 + w^2)^2
+    print $ (w+2)^0
+    print $ (w+2)^1
+    print $ (w+2)^2
+    print $ (w+2)^3
+    print $ (w+2)^4
+    print $ (w+2)^5
+    print $ "----"
+    print $ 2^0
+    print $ 2^w
+    print $ 2^(w*3 + 3)
+    print $ 2^(w^4 + w^3 + 5)
+    print $ 2^(w^w)
+    print $ "----"
+    print $ w^(w^w)
+    print $ (w^w)^2
+    print $ (w^w)^w
+    print $ ((w^w)*3)^2
+    print $ (w^w)^(w^w)
+    print $ "----"
+    print $ (w*w+2)^w
+    print $ w*(w^2)
 
 
 -- main = do
--- ordinals2.hs
+++ ordinals2.hs
@@ -1,8 +1,14 @@
 
+-- An implementation of ordinal arithmetic
+-- Liquid Haskell is used to require all Ordinals to be in Cantor Normal Form
+    -- Comments like {-@ ... @-} are for LH
+    -- To type-check with LH, run: liquid ordinals.hs
+
+----------------------------------------------------------------  
+
 module O where
 
 {-@ LIQUID "--reflection" @-}
--- {-@ LIQUID "--ple" @-}
 
 -- import Language.Haskell.Liquid.NewProofCombinators
 import NewProofCombinators
@@ -32,7 +38,7 @@ normal (Ord a n b) = (normal a) && (n > 0) && (normal b) && (case b of
 
 ----------------------------------------------------------------
 
--- TEMPORARY:
+-- to distinguish from Ints:
 natStyle x = "[" ++ x ++ "]"
 
 instance Show Ordinal where
@@ -119,10 +125,11 @@ op_comp x@(Ord a0 n0 b0) y@(Ord a1 n1 b1) = (x `comp` y == op (y `comp` x))
 
 ----------------------------------------------------------------
 
+-- Based On: https://en.wikipedia.org/wiki/Ordinal_arithmetic#Cantor_normal_form
 instance Num Ordinal where
     (+) x y = Zero -- add (norm x) (norm y) ???
-    (-) _ _ = Zero
-    (*) _ _ = Zero
+    (-) x y = Zero -- sub (norm x) (norm y) ???
+    (*) x y = Zero -- mul (norm x) (norm y) ???
     abs x = x
     signum x = (Ord Zero 1 Zero)
     fromInteger = fromInteger' -- nat2ord . abs' . fromIntegral
@@ -134,7 +141,6 @@ nat2ord' 0 = Zero
 nat2ord' p = Ord Zero p Zero
 {-@ normal_nat :: n:Nat -> {normal (nat2ord' n)} @-}
 normal_nat :: Int -> ()
-normal_nat 0 = normal (nat2ord' 0) === normal Zero *** QED
 normal_nat p = normal (nat2ord' p) === normal Zero *** QED
 {-@ nat2ord :: Nat -> NFOrd @-}
 nat2ord n = (nat2ord' n) `withProof` (normal_nat n)
@@ -152,26 +158,91 @@ fromInteger' = nat2ord . abs' . fromIntegral
 add' :: Ordinal -> Ordinal -> Ordinal
 add' x Zero = x
 add' Zero y = y
-add' x@(Ord a0 n0 b0) y@(Ord a1 n1 b1) = case (comp a1 a0) of
+add' x@(Ord a0 n0 b0) y@(Ord a1 n1 b1) = case (a1 `comp` a0) of
     GT -> y
     LT -> (Ord a0 n0 (add' b0 y))
     EQ -> (Ord a0 (n0+n1) (add' b0 b1))
-{-@ normal_add :: x:NFOrd -> y:NFOrd -> {normal (add' x y)} / [(size x), (size y)] @-}
+{-@ normal_add :: x:NFOrd -> y:NFOrd -> {normal (add' x y)} @-}
 normal_add :: Ordinal -> Ordinal -> ()
 normal_add x y@Zero = normal (add' x y) *** QED
 normal_add x@Zero y = normal (add' x y) *** QED
 normal_add x@(Ord a0 n0 b0) y@(Ord a1 n1 b1) = normal (add' x y) 
-    === (case (comp a1 a0) of
+    === (case (a1 `comp` a0) of
             GT -> (normal y)
             LT -> (normal (Ord a0 n0 (add' b0 y)))
             EQ -> (normal (Ord a0 (n0+n1) (add' b0 b1))))
-    ==? True ? ((normal x == True *** QED)
-                 &&& (normal y == True *** QED)
+    ==? True ? ((normal x *** QED)
+                 &&& (normal y *** QED)
                  &&& eq_is_eq a1 a0
                  &&& normal_add b0 b1
                  &&& normal_add b0 y) *** QED
 {-@ add :: NFOrd -> NFOrd -> NFOrd @-}
 add x y = (add' x y) `withProof` (normal_add x y)
+
+----
+
+{-@ reflect sub' @-}
+sub' :: Ordinal -> Ordinal -> Ordinal
+sub' x Zero = x
+sub' Zero y = Zero
+sub' x@(Ord a0 n0 b0) y@(Ord a1 n1 b1) = case (a0 `comp` a1) of
+    LT -> Zero
+    GT -> x
+    EQ -> case (n0 `compInt` n1) of
+        LT -> Zero
+        GT -> (Ord a0 (n0-n1) b0)
+        EQ -> (sub' b0 b1)
+{-@ normal_sub :: x:NFOrd -> y:NFOrd -> {normal (sub' x y)} @-}
+normal_sub :: Ordinal -> Ordinal -> ()
+normal_sub x y@Zero = normal (sub' x y) *** QED
+normal_sub x@Zero y = normal (sub' x y) *** QED
+normal_sub x@(Ord a0 n0 b0) y@(Ord a1 n1 b1) = normal (sub' x y) 
+    === (case (a0 `comp` a1) of
+            LT -> normal Zero
+            GT -> normal x
+            EQ -> case (n0 `compInt` n1) of
+                LT -> normal Zero
+                GT -> normal (Ord a0 (n0-n1) b0)
+                EQ -> normal (sub' b0 b1))
+    ==? True ? ((normal x *** QED)
+                 &&& (normal y *** QED)
+                 &&& normal_sub b0 b1) *** QED
+{-@ sub :: NFOrd -> NFOrd -> NFOrd @-}
+sub x y = (sub' x y) `withProof` (normal_sub x y)
+
+----
+
+{-@ reflect mul' @-}
+{-@ mul' :: x:Ordinal -> y:Ordinal -> Ordinal / [(size x), (size y)] @-}
+mul' :: Ordinal -> Ordinal -> Ordinal
+mul' x Zero = Zero
+mul' Zero x = Zero
+mul' (Ord a0 n0 b0) (Ord Zero n1 Zero) = (Ord a0 (n0*n1) b0)
+mul' (Ord a0 n0 b0) (Ord a1 n1 Zero) = (Ord (add' a0 a1) n1 Zero)
+mul' x (Ord a1 n1 b1) = (add' (mul' x (Ord a1 n1 Zero)) (mul' x b1))
+{-@ normal_mul :: x:NFOrd -> y:NFOrd -> {normal (mul' x y)} / [(size x), (size y)] @-}
+normal_mul :: Ordinal -> Ordinal -> ()
+normal_mul x y@Zero = normal (mul' x y) *** QED
+normal_mul x@Zero y = normal (mul' x y) *** QED
+normal_mul x@(Ord a0 n0 b0) y@(Ord Zero n1 Zero) = normal (mul' x y) 
+    === normal (Ord a0 (n0*n1) b0)
+    ==? True ? ((normal x *** QED) &&& (normal y *** QED)) *** QED
+normal_mul x@(Ord a0 n0 b0) y@(Ord a1 n1 Zero) = normal (mul' x y) 
+    === normal (Ord (add' a0 a1) n1 Zero)
+    ==? True ? ((normal x *** QED) &&& (normal y *** QED) &&& normal_add a0 a1) 
+    *** QED 
+normal_mul x y@(Ord a1 n1 b1) = normal (mul' x y) 
+    === normal (add' (mul' x (Ord a1 n1 Zero)) (mul' x b1))
+    ==? True ? ((normal x *** QED) 
+            &&& (normal y *** QED)
+            &&& (normal Zero *** QED)
+            &&& (normal (Ord a1 n1 Zero) *** QED)
+            &&& normal_mul x (Ord a1 n1 Zero)
+            &&& normal_mul x b1
+            &&& normal_add (mul' x (Ord a1 n1 Zero)) (mul' x b1))
+    *** QED
+{-@ mul :: NFOrd -> NFOrd -> NFOrd @-}
+mul x y = (mul' x y) `withProof` (normal_mul x y)
 
 ----------------------------------------------------------------
 
